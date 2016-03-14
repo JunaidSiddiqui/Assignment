9c00740efaa6b82df9ac0dd30c4f1383ec1efe95
hmundh123 <harinder.mundh@gmail.com>
2016-03-14 13:48:45 -0400

    Site Map Page for Hajujo's index/home page
    Created a site map page that we are using to build out our home page for Hajujo.

---
+++ template-map.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ *
+ * Template Name: Hajujo's Site Map Page
+ *
+ * @package underscores
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+        
+						<h1>Welcome to Hajujo's!</h1>
+						
+                        <img src="img/cooking.jpg" alt="Cooking Picture" />
+						
+                        <p>HAJUJO is a family owned Mediterranean restaurant located in the heart of Mississauga that offers a fine dining cuisine offering authentic flavors and recipes reigning from the Middle East. Our extensive menu allows guests to enjoy an array of meal choices ranging anywhere from traditional chicken shawarmas to deep fried calamari with a side of baba ganoush!</p>
+						<p><strong><i>Authentic Mediterranean Fine Dining Experience in Mississauga</i></strong></p>
+						
+						<h4>Hours of Operation</h4>
+						<ul>
+							<li><strong>Monday:</strong> 11:00am - 11:00pm</li>
+							<li><strong>Tuesday:</strong>11:00am - 11:00pm</li>
+							<li><strong>Wedesday:</strong>11:00am - 11:00pm</li>
+							<li><strong>Thursday:</strong>11:00am - 11:00pm</li>
+							<li><strong>Friday:</strong>11:00am - 1:00am</li>
+							<li><strong>Saturday:</strong>11:00am - 1:00am</li>
+                            <li><strong>Sunday:</strong>11:00am - 8:00pm></li>
+						</ul>
+			
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
