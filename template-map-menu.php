4f91ebe028ff4abca39549e763e9dae04bad7a65
hmundh123 <harinder.mundh@gmail.com>
2016-03-16 14:13:41 -0400

    Created a custom menu page
    Created a custom template map page for Hajujo's menu page

---
+++ template-map-menu.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * Template Name: Hajujo's - Menu Page
+ *
+ * @package underscores
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+        
+				<h1>Hajujo's Menu</h1>
+						
+                <div id="menucontainer">
+                	<div class="menuleft">
+                    <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/cooking.jpg"/ width= "300" height= "200">
+                    <h2>Chicken Shawarma</h2>
+                    <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/cooking.jpg"/ width= "300" height= "200">
+                    <h2>Chicken Shawarma</h2>
+                    <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/cooking.jpg"/ width= "300" height= "200">
+                    <h2>Chicken Shawarma</h2>
+                    </div>
+                    
+                    <div class="menuright">
+                    <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/cooking.jpg"/ width= "300" height= "200">
+                    <h2>Chicken Shawarma</h2>
+                    <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/cooking.jpg"/ width= "300" height= "200">
+                    <h2>Chicken Shawarma</h2>
+                    <img src="<?php echo bloginfo('stylesheet_directory'); ?>/img/cooking.jpg"/ width= "300" height= "200">
+                    <h2>Chicken Shawarma</h2>
+                    </div>
+                
+                </div>     
+			
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
