f5da7ecf5465c2f2ecd1ea536dfe5bbec1202939
josefadamu <joseph.adamu@mail.utoronto.ca>
2016-03-17 02:23:33 -0400

    Added files via upload

---
+++ slider.php
@@ -0,0 +1,34 @@
+<div id="mySlider" class="carousel slide" data-ride="carousel">
+
+<!--indicators-->
+
+	<ol class="carousel-indicators">
+		<li data-target="#mySlider" data-slide-to="0" class="active"></li>
+		<li data-target="#mySlider" data-slide-to="1"></li></ol>
+
+<div class="carousel-inner">
+	<div class="item active">
+
+		<img src="http://www.placehold.it/800x480&text=Slide+1" alt="">
+		<div class="carousel-caption">
+
+		<h1>Slide 1</h1>
+		</div>
+
+	</div>
+
+	<div class="item">
+
+		<img src="http://placehold.it/800x480&text=Slide+2" alt="">
+		<div class="carousel-caption">
+
+		<h1>Slide 2</h1>
+
+		</div>
+
+
+
+
+
+
+</div>
